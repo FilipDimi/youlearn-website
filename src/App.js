4f42cbc9a4d8ddbd6e8fb56d52be75b747d4c61c
dimitr36 <dimitr36@msu.edu>
2023-05-05 21:01:02 -0400

    Warning fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
-import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
 import LearnScreen from "./screens/LearnScreen";
 
